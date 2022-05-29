9aa5c815f28b78257accb6a1dde3385ceb4eae00
javiarias <fezkip@gmail.com>
2022-05-29 15:43:23 +0100

    Añadida página de trabajo

--- docs/js/projects.js
+++ docs/js/projects.js
@@ -1,12 +1,60 @@
 const teamGames = [
     {
-        companyName: "Cloud Imperium",
-        role: "UI Programmer I",
-        imageSrc: "images/workplace/cig.png",
-        //download: "https://freestylers-studio.itch.io/district-dance-battle-sdl",
-        //source: "https://github.com/freesstylers/District-Dance-Battle",
-        description: "Work on Star Citizen and Squadron 42",
-        //extra: "Winner of the <b> Best Game Award </b> at FIMP 2019"
+        projectName: "District Dance Battle",
+        projectType: "SDL (C++)",
+        imageSrc: "images/games/ddb.png",
+        download: "https://freestylers-studio.itch.io/district-dance-battle-sdl",
+        source: "https://github.com/freesstylers/District-Dance-Battle",
+        description: "A rhythm game inspired by Dance Dance Revolution, but with a twist!",
+        extra: "Winner of the <b> Best Game Award </b> at FIMP 2019"
+    },
+    {
+        projectName: "Mauriçius Nativitatem",
+        projectType: "Unity (C#)",
+        imageSrc: "images/games/mauricius.png",
+        download: "https://freestylers-studio.itch.io/mauricius-nativitatem",
+        source: "https://github.com/freesstylers/JamOn",
+        description: "A small rhythm game inspired by the Patapon series",
+        extra: "Won <b> second place </b> at the 2021 JamOn game jam"
+    },
+    {
+        projectName: "Chromaturgia",
+        projectType: "Unity (C#)",
+        imageSrc: "images/games/chromaturgia.png",
+        download: "https://javiarias.github.io/Chromaturgia/",
+        source: "https://github.com/javiarias/Chromaturgia",
+        description: "A short puzzle game based around shooting color pellets"
+    },
+    {
+        projectName: "TeFeGe",
+        projectType: "<u>Game</u>: Unity (C#) <br> <u>Servers</u>: Mirror (C#), Express (JS)",
+        imageSrc: "images/games/tefege.png",
+        download: "https://tefege.itch.io/tefege",
+        source: "https://github.com/HoracioStudios/TFG",
+        description: "An online multiplayer twin-stick shooter, with a functioning matchmaking system. <b>Currently offline</b>"
+    },
+    {
+        projectName: "Motor Casa Paco",
+        projectType: 'Custom Game Engine (C++)',
+        imageSrc: "images/games/casapaco.png",
+        source: "https://github.com/freesstylers/Untitled-Motor",
+        description: "A custom 3D game engine made using Ogre3D, FMOD, Bullet Physics and SDL"
+    },
+    {
+        projectName: "Vaya Canicastañazos",
+        projectType: '"Motor Casa Paco" (C++)',
+        imageSrc: "images/games/canicas.png",
+        download: "https://freestylers-studio.itch.io/vaya-canicastanhazos",
+        source: "https://github.com/freesstylers/Vaya-Canicastanyazos",
+        description: "A platformer inspired by the Monkey Ball series"
+    },
+    {
+        projectName: "A Toda Pastilla",
+        projectType: '"Motor Casa Paco" (C++)',
+        imageSrc: "images/games/pastilla.png",
+        download: "https://freestylers-studio.itch.io/a-toda-pastilla",
+        source: "https://github.com/freesstylers/A-Toda-Pastilla",
+        description: "An infinite bullet hell game"
     }
 ];
 
@@ -17,7 +65,7 @@ const personalProjects = [
 document.getElementById("section-work").innerHTML = `
     <div class="container-fluid">
         <div class="d-flex justify-content-center fh5co-heading-white">
-            <h2>My work History</h2>
+            <h2>Games/Projects I've worked on</h2>
         </div>
         <div class="container-fluid">
             <div class="row">
@@ -29,8 +77,8 @@ document.getElementById("section-work").innerHTML = `
                             <img src="${project.imageSrc}" class="card-img-top" style="padding:5%;"/>
 
                             <div class="card-body">
-                                <h4 class="card-title coral">${project.companyName} </h4>
-                                <h5 class="card-text" style="color:black;">${project.role} </h5>
+                                <h4 class="card-title coral">${project.projectName} </h4>
+                                <h5 class="card-text" style="color:black;">${project.projectType} </h5>
                                 <h5 class="card-text" style="color:black;">${project.description} </h5>
                                 <h5 class="card-text" style="color:black;">${(project.extra == undefined) ? "" : project.extra} </h5>
                             </div>
@@ -68,7 +116,7 @@ if(personalProjects.length > 0)
                         <img src="${project.imageSrc}" />
                         
                         <figcaption>${project.projectName} 
-                        <p>${project.company} </p>
+                        <p>${project.projectType} </p>
                         <p>${project.description} </p>
                         <a target="_blank" href="${project.projectLink}">
                         <button><span>Play</span></button>
